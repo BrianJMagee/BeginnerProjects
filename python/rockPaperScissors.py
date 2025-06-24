5b35f5e3f9591aa4db9f1d4411baca6d41b4c467
BrianJMagee <brianmagee300@gmail.com>
2025-06-24 15:46:22 +0100

    Formatted the file

--- python/rockPaperScissors.py
+++ python/rockPaperScissors.py
@@ -14,24 +14,37 @@ while play == True:
     for i in range(3, 0, -1):
         print(i)
         time.sleep(1)
-    
+
     choices = ("Rock", "Paper", "Scissors")
 
     userChoice = str(input("GO: "))
     computerChoice = random.choice(choices)
     print(f"Computer chose {computerChoice}")
-    
-    if userChoice == "r" and computerChoice == "Rock" or userChoice == "p" and computerChoice == "Paper" or userChoice == "s" and computerChoice == "Scissors":
+
+    if (
+        userChoice == "r"
+        and computerChoice == "Rock"
+        or userChoice == "p"
+        and computerChoice == "Paper"
+        or userChoice == "s"
+        and computerChoice == "Scissors"
+    ):
         again = input("A tie! Shall we go again (y/n): ")
-    elif userChoice == "r" and computerChoice == "Scissors" or userChoice == "p" and computerChoice == "Rock" or userChoice == "s" and computerChoice == "Paper":
+    elif (
+        userChoice == "r"
+        and computerChoice == "Scissors"
+        or userChoice == "p"
+        and computerChoice == "Rock"
+        or userChoice == "s"
+        and computerChoice == "Paper"
+    ):
         again = input("You win! Shall we go again (y/n): ")
     else:
         again = input("The computer wins! Shall we go again (y/n): ")
 
-    
     if again == "y":
         break  # valid, continue game loop
-        
+
     elif again == "n":
         play = False
         print("Thanks for playing!")
