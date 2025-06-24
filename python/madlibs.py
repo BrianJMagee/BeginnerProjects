cfee580c69a057b913c146d028d982691357681f
BrianJMagee <brianmagee300@gmail.com>
2025-06-24 15:02:31 +0100

    Created madlibs.py
    Simple madlib program

--- python/madlibs.py
+++ python/madlibs.py
@@ -1,1 +1,18 @@
+place = input("Please enter a place name: ")
+adj1 = input("Please enter an adjective: ")
+noun1 = input("Please enter a noun: ")
+adj2 = input("Please enter an adjective: ")
+verb1 = input("Please enter a verb ending in ing: ")
+bodyPart = input("Please enter a body part: ")
+noun2 = input("Please enter a plural noun: ")
+verb2 = input("Please enter a verb ending in ing: ")
+noun3 = input("Please enter a plural noun: ")
+adj3 = input("Please enter an adjective: ")
 
+
+
+
+
+print(f"Yesterday, I went to the {place} and saw the most {adj1} {noun1} ever. It was wearing a {adj2} hat and {verb1} down the") 
+print(f"street like it owned the place. I couldn't believe my {bodyPart}! Then, a group of {noun2} started {verb2} after it, holding") 
+print(f"{noun3} in their hands. It was the most {adj3} day of my life.")
