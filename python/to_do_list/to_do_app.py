45c0c9ef0d9622981232ce7860f0e89af2f5a20d
BrianJMagee <brianmagee300@gmail.com>
2025-07-01 14:47:38 +0100

    modified

--- python/to_do_list/to_do_app.py
+++ python/to_do_list/to_do_app.py
@@ -1,16 +1,40 @@
-class To_Do_App:
-    def __init__(self):
-        print("Hello world")
+from to_do_list import To_Do_List
 
-    def run():
-        #The main loop (menu, user interaction)
-        pass
+class To_Do_App:
+    def run(self):
+        tasks = []
+        my_list = To_Do_List(tasks=tasks)
+
+        run = True
+        while run:
+            #print menu
+            self.print_menu()
+
+            #get choice
+            choice = self.get_input()
+
+            #handle choice
+            self.handle_choice(choice)
+
 
     def print_menu():
         #Shows options (add, remove, list, quit, etc.)
-        pass
+        print("********To-Do_List********")
+        print("Your options are:")
+        print("1. Add task")
+        print("2. Remove task")
+        print("**************************")
+
+    def get_input():
+        try:
+            choice = int(input("Please enter your choice: "))
+            return choice
+
+        except ValueError as e:
+            print(f"Error: {e}")
 
     def handle_choice(choice):
         #Dispatches to ToDoList methods
-        pass
+        match choice:
+            case 1: 
 
